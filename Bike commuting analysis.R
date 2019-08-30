d77c91b351e8d41156938a166f2da667d7cb9303
Harald Kliems <hmk701@pediatrics.wisc.edu>
2019-08-30 15:40:52 -0500

    starting to refactor to use tidycensus. very messy and only started!

--- Bike commuting analysis.R
+++ Bike commuting analysis.R
@@ -1,8 +1,6 @@
-library(acs)
-library(ggplot2)
-library(reshape2)
-library(gridExtra)
+library(tidycensus)
 library(tidyverse)
+library(purrr)
 
 ######################
 ######################
@@ -10,9 +8,58 @@ library(tidyverse)
 ######################
 ######################
 
+v17 <- load_variables(2017, "acs1/subject", cache = TRUE)
 
-## install API key
-APIKEY <- api.key.install("7dbf424d492b694ca9de38c3c2b90867d55086be")
+
+#define variables for total number commuters and total bike commuters
+vars <- c("B08006_001", "B08006_014")
+
+year <- c(seq(2016, 2017, by=1))
+
+
+places <- get_acs(geography = "place", table = "S0802", state = "WI", year = 2017)
+
+us <- unique(fips_codes$state)[1:51]
+
+# Process all tracts by state
+df <- map_df(year, function(x) {
+  get_acs(geography = "state", variables = "B08006_001", year = x) %>% mutate(year = x)
+})
+
+df2010 <- map_df(us, function(x) {
+  get_acs(geography = "tract", variables = vars, state = x, year = 2010, output = "wide", cache_table = TRUE)
+})
+
+df_all <- inner_join(df, df2010, by = ("GEOID"))
+
+bike_growth <- df_all %>%
+  mutate(share_2010 = B08006_014E.y / B08006_001E.y, share_2017 = B08006_014E.x / B08006_001E.x, growth = share_2017 - share_2010) %>%
+  filter(share_2010 > 0.03, B08006_001E.x > 500, str_detect(NAME.x, 'Dane')) %>%
+  select(GEOID, NAME.x, B08006_001E.x, share_2010:growth) %>%
+  arrange(desc(growth))
+
+bike_growth <- df_all %>%
+  mutate(share_2010 = B08006_014E.y / B08006_001E.y, share_2017 = B08006_014E.x / B08006_001E.x, growth = share_2017 - share_2010) %>%
+  filter(share_2010 > 0.03, B08006_001E.x > 500) %>%
+  select(GEOID, NAME.x, B08006_001E.x, share_2010:growth) %>%
+  arrange(desc(growth))
+
+#calculate bike mode share and sort descending
+hi_bike <- df %>%
+  filter(B08006_001E > 500) %>%
+  mutate(bike_share = B08006_014E/B08006_001E) %>%
+  filter(bike_share > 0.1) %>%
+  arrange(desc(bike_share))
+
+hi_bike <- df2010 %>%
+  filter(B08006_001E > 500) %>%
+  mutate(bike_share = B08006_014E/B08006_001E) %>%
+  filter(bike_share > 0.1) %>%
+  arrange(desc(bike_share))
+
+view(hi_bike)
+
+
 
 ## Looking up the right geography
 geo.lookup(state="WI", county="Dane", place="Madison")
